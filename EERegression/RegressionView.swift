fe1d19e37472366eec876e9e20d1c2df610e0f67
Erk EKİN <eekin@anadolu.edu.tr>
2015-11-22 15:57:40 +0200

    grid added

--- EERegression/RegressionView.swift
+++ EERegression/RegressionView.swift
@@ -12,7 +12,54 @@ class RegressionView: UIView {
     
     var points:[CGPoint] = []
     var modelLine:CAShapeLayer?
+    let gridWidth: CGFloat = 0.5
+    var columns: Int = 50
     
+    init(frame: CGRect, columns: Int) {
+        // Set size of grid
+        self.columns = columns - 1
+        super.init(frame: frame)
+        
+        // Set view to be transparent
+        self.opaque = false;
+        self.backgroundColor = UIColor(white: 0.0, alpha: 0.0);
+    }
+    
+
+    override func drawRect(rect: CGRect) {
+        let context: CGContextRef = UIGraphicsGetCurrentContext()!
+        CGContextSetLineWidth(context, gridWidth)
+        CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+        
+        // Calculate basic dimensions
+        let columnWidth: CGFloat = self.frame.size.width / (CGFloat(self.columns) + 1.0)
+        let rowHeight: CGFloat = columnWidth;
+        let numberOfRows: Int = Int(self.frame.size.height)/Int(rowHeight);
+        
+        // ---------------------------
+        // Drawing column lines
+        // ---------------------------
+        for i in 1...self.columns {
+            let startPoint: CGPoint = CGPoint(x: columnWidth * CGFloat(i), y: 0.0)
+            let endPoint: CGPoint = CGPoint(x: startPoint.x, y: self.frame.size.height)
+            
+            CGContextMoveToPoint(context, startPoint.x, startPoint.y);
+            CGContextAddLineToPoint(context, endPoint.x, endPoint.y);
+            CGContextStrokePath(context);
+        }
+        
+        // ---------------------------
+        // Drawing row lines
+        // ---------------------------
+        for j in 1...numberOfRows {
+            let startPoint: CGPoint = CGPoint(x: 0.0, y: rowHeight * CGFloat(j))
+            let endPoint: CGPoint = CGPoint(x: self.frame.size.width, y: startPoint.y)
+            
+            CGContextMoveToPoint(context, startPoint.x, startPoint.y);
+            CGContextAddLineToPoint(context, endPoint.x, endPoint.y);
+            CGContextStrokePath(context);
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
@@ -89,34 +136,7 @@ class RegressionView: UIView {
         let layer = CAShapeLayer()
         layer.path = UIBezierPath(roundedRect: CGRect(x: point.x - 2.5, y: point.y - 2.5, width: 5, height: 5), cornerRadius: 2.5).CGPath
         layer.fillColor = UIColor.redColor().CGColor
-        return layer
-        
-        //
-        //        let rwColor = UIColor(red: 11/255.0, green: 86/255.0, blue: 14/255.0, alpha: 1.0)
-        //        let rwPath = UIBezierPath()
-        //        let rwLayer = CAShapeLayer()
-        //
-        //        rwPath.moveToPoint(CGPointMake(0.22, 124.79))
-        //        rwPath.addLineToPoint(CGPointMake(0.22, 249.57))
-        //        rwPath.addLineToPoint(CGPointMake(124.89, 249.57))
-        //        rwPath.addLineToPoint(CGPointMake(249.57, 249.57))
-        //        rwPath.addLineToPoint(CGPointMake(249.57, 143.79))
-        //        rwPath.addCurveToPoint(CGPointMake(249.37, 38.25), controlPoint1: CGPointMake(249.57, 85.64), controlPoint2: CGPointMake(249.47, 38.15))
-        //        rwPath.addCurveToPoint(CGPointMake(206.47, 112.47), controlPoint1: CGPointMake(249.27, 38.35), controlPoint2: CGPointMake(229.94, 71.76))
-        //        rwPath.addCurveToPoint(CGPointMake(163.46, 186.84), controlPoint1: CGPointMake(182.99, 153.19), controlPoint2: CGPointMake(163.61, 186.65))
-        //        rwPath.addCurveToPoint(CGPointMake(146.17, 156.99), controlPoint1: CGPointMake(163.27, 187.03), controlPoint2: CGPointMake(155.48, 173.59))
-        //        rwPath.addCurveToPoint(CGPointMake(128.79, 127.08), controlPoint1: CGPointMake(136.82, 140.43), controlPoint2: CGPointMake(129.03, 126.94))
-        //        rwPath.addCurveToPoint(CGPointMake(109.31, 157.77), controlPoint1: CGPointMake(128.59, 127.18), controlPoint2: CGPointMake(119.83, 141.01))
-        //        rwPath.addCurveToPoint(CGPointMake(89.83, 187.86), controlPoint1: CGPointMake(98.79, 174.52), controlPoint2: CGPointMake(90.02, 188.06))
-        //        rwPath.addCurveToPoint(CGPointMake(56.52, 108.28), controlPoint1: CGPointMake(89.24, 187.23), controlPoint2: CGPointMake(56.56, 109.11))
-        //        rwPath.addCurveToPoint(CGPointMake(64.02, 102.25), controlPoint1: CGPointMake(56.47, 107.75), controlPoint2: CGPointMake(59.24, 105.56))
-        //        rwPath.addCurveToPoint(CGPointMake(101.42, 67.57), controlPoint1: CGPointMake(81.99, 89.78), controlPoint2: CGPointMake(93.92, 78.72))
-        //        rwPath.addCurveToPoint(CGPointMake(108.38, 30.65), controlPoint1: CGPointMake(110.28, 54.47), controlPoint2: CGPointMake(113.01, 39.96))
-        //        rwPath.addCurveToPoint(CGPointMake(10.35, 0.41), controlPoint1: CGPointMake(99.66, 13.17), controlPoint2: CGPointMake(64.11, 2.16))
-        //        rwPath.addLineToPoint(CGPointMake(0.22, 0.07))
-        //        rwPath.addLineToPoint(CGPointMake(0.22, 124.79))
-        //        rwPath.closePath()
-    }
+        return layer    }
     
     
 }
@@ -133,13 +153,4 @@ class RegressionView: UIView {
 //    rwLayer.lineWidth = 1.0
 //    rwLayer.miterLimit = 10.0
 //    rwLayer.strokeColor = rwColor.CGColor
-//}
-//
-//override func viewDidLoad() {
-//    super.viewDidLoad()
-//
-//    // 4
-//    setUpRWPath()
-//    setUpRWLayer()
-//    someView.layer.addSublayer(rwLayer)
 //}
